a832331704b2c98964280b85daf3c17a51425fc0
sandeep <sandeepkrpatel2002@gmail.com>
2023-03-09 19:42:18 +0530

    backend done

--- app.js
+++ app.js
@@ -1,15 +1,117 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
 const rrr = path.join(__dirname, "/");
 // const { a } = require("./a.js");
 // a();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(rrr));
+mongoose.connect(
+  "mongodb://localhost:27017/newCollection",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (e, r) => {
+    console.log("connection succ");
+  }
+);
+
+const contactSchema = {
+  data: String,
+};
+const Contact = mongoose.model("Contact", contactSchema);
+const newsSchema = {
+  heading: String,
+  detail: String,
+};
+const newsContact = mongoose.model("news", newsSchema);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
   // setTimeout(() => {
   //     window.location.href="./index.html";
   // }, 5000);
+});
+app.post("/researchsave", async (req, res) => {
+  console.log(req.body);
+  const contact = new Contact({
+    data: req.body.text,
+  });
+  contact.save(function (err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log("savedata");
+    }
+  });
+});
+app.post("/researchfind", async (req, res) => {
+  let array = new Array();
+  await Contact.find()
+    .populate()
+    .then((p) => {
+      p.forEach((t) => {
+        // console.log(t.data);
+        array.push(t.data);
+      });
+    })
+    .catch((error) => console.log(error));
+  // console.log("..array=", ...array);
+  res.send(array);
+});
+app.post("/researchdel", async (req, res) => {
+  console.log(req.body);
+  Contact.deleteOne({ data: req.body.text }, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("reserchremove");
+    }
+  });
+});
+app.post("/newssave", async (req, res) => {
+  console.log(req.body);
+  const contact = new newsContact({
+    heading: req.body.heading,
+    detail: req.body.detail,
+  });
+  contact.save(function (err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log("savedata");
+    }
+  });
+});
+app.post("/newsfind", async (req, res) => {
+  let array = new Array();
+  await newsContact
+    .find()
+    .populate()
+    .then((p) => {
+      console.log(p);
+      res.send(p);
+      // p.forEach((t) => {
+      //   console.log(t);
+      //   array.push(t.data);
+      // });
+    })
+    .catch((error) => console.log(error));
+  // console.log("..array=", ...array);
+});
+app.post("/newsdel", async (req, res) => {
+  console.log(req.body);
+  newsContact.deleteOne({ heading: req.body.text }, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("newsremove");
+    }
+  });
 });
 app.listen(3000, () => {
   console.log("sandeep patel");
--- index.js
+++ index.js
@@ -1,49 +1,187 @@
-var requirejs = require("requirejs");
-requirejs.config({
-  //Pass the top-level main.js/index.js require
-  //function to requirejs so that node modules
-  //are loaded relative to the top-level JS file.
-  nodeRequire: require,
-});
 let h5 = document.getElementById("researchh5");
 let data = ["Ram", "Shyam", "Sita", "Gita"];
 document.getElementById("research").addEventListener("click", () => {
   let text = prompt("Please Enter Your Password", "");
   let hr = document.createElement("hr");
   h5.appendChild(hr);
-  var MongoClient = requirejs("mongodb").MongoClient;
-  var url = "mongodb://localhost:27017/";
 
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    var myobj = text;
-    dbo.collection("customers").insertOne(myobj, function (err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-    });
-  });
   let li = document.createElement("a");
   li.innerText = text;
   h5.appendChild(li);
-  //   data.forEach((item) => {
-  //     let hr = document.createElement("hr");
-  //     h5.appendChild(hr);
-  //     let li = document.createElement("a");
-  //     li.innerText = item;
-  //     h5.appendChild(li);
-  //   });
+  let data = { text };
+  const req = new XMLHttpRequest();
+  const baseUrl = "http://localhost:3000/researchsave";
+  const urlParams = data;
+
+  req.open("POST", baseUrl, true);
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send(JSON.stringify(urlParams));
+
+  req.onreadystatechange = async function () {
+    // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const nodedata = JSON.parse(this.responseText);
+      console.log("iiiii", nodedata);
+    }
+  };
+
+  // data.forEach((item) => {
+  //   let hr = document.createElement("hr");
+  //   h5.appendChild(hr);
+  //   let li = document.createElement("a");
+  //   li.innerText = item;
+  //   h5.appendChild(li);
+  // });
 });
 ////////////////////////////////////////////newssssss
 let newsul = document.getElementById("aa");
 
 document.getElementById("newspen").addEventListener("click", () => {
   let text = prompt("heading", "");
-  let li = document.createElement("li");
-  let h6 = document.createElement("h6");
-  h6.innerText = text;
-  li.appendChild(h6);
-  li.append("nsdncsdifnf");
-  newsul.prepend(li);
+
+  let textd = prompt("detail", "");
+  //////////////////////////////
+  if (text.length && textd.length) {
+    let li = document.createElement("li");
+    let h6 = document.createElement("h6");
+    let p = document.createElement("p");
+    p.setAttribute("class", "hi");
+
+    h6.innerText = text;
+
+    li.appendChild(h6);
+
+    p.append(textd);
+    li.append(p);
+    newsul.prepend(li);
+
+    /////////////////////////
+
+    let data = { heading: text, detail: textd };
+    const req = new XMLHttpRequest();
+    const baseUrl = "http://localhost:3000/newssave";
+    const urlParams = data;
+
+    req.open("POST", baseUrl, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify(urlParams));
+
+    req.onreadystatechange = async function () {
+      // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const nodedata = JSON.parse(this.responseText);
+        console.log("iiiii", nodedata);
+      }
+    };
+  }
 });
+let researchfinddata = function () {
+  const req = new XMLHttpRequest();
+  const baseUrl = "http://localhost:3000/researchfind";
+  let data = {};
+  const urlParams = data;
+
+  req.open("POST", baseUrl, true);
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send(JSON.stringify(urlParams));
+
+  req.onreadystatechange = async function () {
+    // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const nodedata = JSON.parse(this.responseText);
+      // console.log("iiiii", nodedata);
+      nodedata.forEach((element) => {
+        console.log(element);
+        let hr = document.createElement("hr");
+        h5.appendChild(hr);
+
+        let li = document.createElement("a");
+        li.innerText = element;
+        h5.appendChild(li);
+      });
+    }
+  };
+};
+researchfinddata();
+
+let newsfinddata = function () {
+  const req = new XMLHttpRequest();
+  const baseUrl = "http://localhost:3000/newsfind";
+  let data = {};
+  const urlParams = data;
+
+  req.open("POST", baseUrl, true);
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send(JSON.stringify(urlParams));
+
+  req.onreadystatechange = async function () {
+    // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const nodedata = JSON.parse(this.responseText);
+      // console.log("iiiii", nodedata);
+
+      nodedata.forEach((element) => {
+        let li = document.createElement("li");
+        let h6 = document.createElement("h6");
+        h6.setAttribute("class", "news_title");
+        let p = document.createElement("p");
+        p.setAttribute("class", "hi");
+        console.log(element);
+        h6.innerText = element.heading;
+        console.log(element.heading);
+        li.appendChild(h6);
+
+        p.append(element.detail);
+        li.append(p);
+        newsul.prepend(li);
+      });
+    }
+  };
+};
+
+document.getElementById("researchdel").addEventListener("click", () => {
+  let text = prompt("whichone want to delete", "");
+  console.log(text.length);
+  if (text.length) {
+    let data = { text };
+    const req = new XMLHttpRequest();
+    const baseUrl = "http://localhost:3000/researchdel";
+    const urlParams = data;
+
+    req.open("POST", baseUrl, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify(urlParams));
+
+    req.onreadystatechange = async function () {
+      // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const nodedata = JSON.parse(this.responseText);
+        console.log("iiiii", nodedata);
+      }
+    };
+  }
+});
+/////////////////////////////
+document.getElementById("newsdel").addEventListener("click", () => {
+  let text = prompt("whichone want to delete", "");
+  console.log(text.length);
+  if (text && text.length) {
+    let data = { text };
+    const req = new XMLHttpRequest();
+    const baseUrl = "http://localhost:3000/newsdel";
+    const urlParams = data;
+
+    req.open("POST", baseUrl, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify(urlParams));
+
+    req.onreadystatechange = async function () {
+      // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const nodedata = JSON.parse(this.responseText);
+        console.log("iiiii", nodedata);
+      }
+    };
+  }
+});
+newsfinddata();
